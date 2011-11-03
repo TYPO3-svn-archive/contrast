795a8c27a6608a0daf4e3d6bac35fad0ae8cb872
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-03 21:11:59 +0000

    [TASK] Initial kickstarted version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/contrast/trunk@53674 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "contrast".
+#
+# Auto generated 03-11-2011 22:10
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Contrast Switch',
+	'description' => 'Contrast Switch for web accessible websites. This plugin loads an additional stylesheet and stores the status in the user session.',
+	'category' => 'fe',
+	'author' => 'Oliver Hader',
+	'author_email' => 'oliver.hader@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"499e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"3f08";s:20:"doc/wizard_form.html";s:4:"06d9";}',
+);
+
+?>
