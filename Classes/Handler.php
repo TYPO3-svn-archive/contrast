8ef887e3076efa4f995b8fd1b2ab4d280b082605
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-03 23:58:25 +0000

    [TASK] Initial version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/contrast/trunk@53677 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Handler.php
@@ -0,0 +1,109 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2011 Oliver Hader <oliver.hader@typo3.org>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Handles the contrast view state.
+ */
+class Tx_Contrast_Handler {
+	const NAME = 'tx_contrast';
+
+	/**
+	 * @var array
+	 */
+	protected $status;
+
+	/**
+	 * @var array
+	 */
+	protected $arguments;
+
+	/**
+	 * Creates this object.
+	 */
+	public function __construct() {
+		$this->fetch();
+		$this->arguments = t3lib_div::_GP(self::NAME);
+	}
+
+	/**
+	 * Executes the handler.
+	 *
+	 * @return void
+	 */
+	public function execute() {
+		if (isset($this->arguments['toggle']) && $this->arguments['toggle']) {
+			$this->toggle();
+			$this->persist();
+		}
+
+		if (isset($this->arguments['url']) && $this->arguments['url']) {
+			t3lib_utility_Http::redirect(
+				$this->arguments['url'],
+				t3lib_utility_Http::HTTP_STATUS_307
+			);
+		}
+	}
+
+	/**
+	 * Toggles the status.
+	 *
+	 * @return void
+	 */
+	protected function toggle() {
+		if (isset($this->status['enable'])) {
+			$this->status['enable'] = ($this->status['enable'] ? 0 : 1);
+		} else {
+			$this->status['enable'] = 1;
+		}
+	}
+
+	/**
+	 * Fetches the status from the session data.
+	 *
+	 * @return void
+	 */
+	protected function fetch() {
+		$this->status = (array) $this->getFrontend()->fe_user->getKey('ses', self::NAME);
+	}
+
+	/**
+	 * Persists the status to the session data.
+	 *
+	 * @return void
+	 */
+	protected function persist() {
+		$this->getFrontend()->fe_user->setKey('ses', self::NAME, $this->status);
+		$this->getFrontend()->fe_user->storeSessionData();
+	}
+
+	/**
+	 * Gets the frontend object.
+	 *
+	 * @return tslib_fe
+	 */
+	protected function getFrontend() {
+		return $GLOBALS['TSFE'];
+	}
+}
+?>
--- README.txt
+++ README.txt
@@ -1,2 +1,22 @@
+===============================================================================
+Extension contrast
+===============================================================================
 
-Feel free to add some documentation or simply add a link to the online manual.
+How to use in TypoScript:
+
+* you need to include the static TypoScript of this extension
+* you neet to assign the hanler link at the accordant object, example
+
+	page = PAGE
+	page.999 =< lib.tx_contrast.link
+
+How it works:
+
+* calling the handler writes the status with the key 'tx_contrast' to the user
+  session ($GLOBALS['TSFE']->fe_user->sesData
+* the stylesheet file will be loaded to [PAGE].includeCSS.99999
+* a TypoScript condition ensures that the stylesheet get included if required
+
+===============================================================================
+  (c) 2011 Oliver Hader <oliver.hader@typo3.org> - licensed under the GPLv2+
+===============================================================================
