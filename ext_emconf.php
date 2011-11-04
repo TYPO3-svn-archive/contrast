408a29e34f96c28230720c02fbf4a3ff9ff2d3bf
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-04 12:46:59 +0000

    Fixed bug with missing rel attribute
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/contrast/trunk@53701 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "contrast".
 #
-# Auto generated 04-11-2011 13:42
+# Auto generated 04-11-2011 13:46
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"e896";s:10:"README.txt";s:4:"3e23";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"599e";s:19:"Classes/Handler.php";s:4:"2fb7";s:20:"Classes/Includer.php";s:4:"406b";s:38:"Configuration/TypoScript/constants.txt";s:4:"e382";s:34:"Configuration/TypoScript/setup.txt";s:4:"5d92";s:31:"Resources/Private/locallang.xml";s:4:"340f";s:29:"Resources/Public/contrast.css";s:4:"ed7d";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"0cc9";s:10:"README.txt";s:4:"3e23";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"599e";s:19:"Classes/Handler.php";s:4:"2fb7";s:20:"Classes/Includer.php";s:4:"d6ab";s:38:"Configuration/TypoScript/constants.txt";s:4:"e382";s:34:"Configuration/TypoScript/setup.txt";s:4:"5d92";s:31:"Resources/Private/locallang.xml";s:4:"340f";s:29:"Resources/Public/contrast.css";s:4:"ed7d";}',
 	'suggests' => array(
 	),
 );
